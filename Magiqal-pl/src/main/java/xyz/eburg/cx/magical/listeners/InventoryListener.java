fc2463e79c9c3699a6e14c125c0b801fa8199e03
Cron3x <craipy.exe@gmail.com>
2022-05-17 14:30:23 +0200

    put ring in chest stop flighing after closing

--- Magiqal-pl/src/main/java/xyz/eburg/cx/magical/listeners/InventoryListener.java
+++ Magiqal-pl/src/main/java/xyz/eburg/cx/magical/listeners/InventoryListener.java
@@ -6,10 +6,10 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.inventory.*;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.ItemStack;
+import xyz.eburg.cx.magical.spells.FlightRing;
 import xyz.eburg.cx.magical.spells.MagicSpell;
 import xyz.eburg.cx.magical.utils.ItemUtils;
 
@@ -31,6 +31,27 @@ public class InventoryListener implements Listener {
     player.setAllowFlight(false);
     */
   }
+  @EventHandler
+  public void onInvChange(InventoryCloseEvent event){
+    Player player = (Player) event.getView().getPlayer();
+    ItemStack[] items = player.getInventory().getContents();
+    for (ItemStack item : items){
+      if (item == null) continue;
+      System.out.println("@ InventoryEvent: item ("+item.getType()+") is not null");
+      if (!item.getType().equals(Material.CLOCK)) continue;
+      System.out.println("@ InventoryEvent: item ("+item.getType()+") is a clock");
+      if (!ItemUtils.isMagiqal(item)) continue;
+      System.out.println("@ InventoryEvent: item ("+item.getType()+") is magiqal");
+      if (!ItemUtils.getSpell(item).equals(MagicSpell.FLIGHT)) continue;
+      System.out.println("@ InventoryEvent: item ("+item.getType()+") has Flight Spell");
+      if (!ItemUtils.getAllowFlight(item)) continue;
+      System.out.println("@ InventoryEvent: item ("+item.getType()+") allows flight");
+      new FlightRing(item, player, true);
+      return;
+    }
+    player.setAllowFlight(false);
+  }
+
 
   @EventHandler
   public void onDropChange(PlayerDropItemEvent e){
